0d4a47293bfe78e1acf25d308376c8a675850c9f
Seah Shun Zhou <seahsz@gmail.com>
2025-01-09 11:45:38 +0800

    Initial commit

---
+++ src/main/java/vttp/paf/day21lecture/Day21lectureApplication.java
@@ -0,0 +1,13 @@
+package vttp.paf.day21lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day21lectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day21lectureApplication.class, args);
+	}
+
+}
